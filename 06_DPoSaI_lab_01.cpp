b17b2d50836057717e7efda53a802d0cb7de6144
Maxim Komlik <maxim.komlik@tut.by>
2021-03-28 22:03:33 +0300

    Bug fixes. Performance tests

--- 06_DPoSaI_lab_01.cpp
+++ 06_DPoSaI_lab_01.cpp
@@ -1,4 +1,5 @@
 ﻿#include <cmath>
+#include <ctime>
 #include <iostream>
 
 #include "Fourier.h"
@@ -6,7 +7,7 @@
 typedef double T;
 constexpr double pi = 3.141592653589793238;
 
-constexpr unsigned int sampleNum = 16;
+constexpr unsigned int sampleNum = 4096 * 2;
 constexpr T stride = pi * 2 / sampleNum;
 constexpr unsigned int deepth = sampleNum;
 
@@ -19,45 +20,54 @@ int main()
 		functionSamples[i] = cos(argument * 3) + sin(argument * 2);
 		argument += stride;
 	}
-
-	std::cout << "Input function: " << std::endl;
-	for (unsigned int i = 0; i < functionSamples.size(); i++) {
-		std::cout << i + 1 << ". " << functionSamples[i] << std::endl;
-	}
-
-	{
-		using namespace Fourier::trivial;
-		std::vector<std::complex<T>> frequencies =
-			directTransform(functionSamples, deepth);
-		//std::vector<T> restored = inverseTransform(frequencies, sampleNum);
-
-		std::cout << std::endl << "Direct Fourier transform: " << std::endl;
-		for (unsigned int i = 0; i < frequencies.size(); i++) {
-			std::cout << i + 1 << ". " << frequencies[i].real() << " + " << frequencies[i].imag() << "*i" << std::endl;
-		}
-		//std::cout << std::endl << "Function restoration with inverse Fourier transform: " << std::endl;
-		//for (unsigned int i = 0; i < restored.size(); i++) {
-		//	std::cout << i + 1 << ". " << restored[i] << std::endl;
-		//}
-	}
 	{
 		using namespace Fourier::fast;
-		std::vector<std::complex<T>> frequencies = directTransform(functionSamples, sampleNum);
-		std::cout << std::endl << "Direct fast Fourier transform: " << std::endl;
-		for (unsigned int i = 0; i < frequencies.size(); i++) {
-			std::cout << i + 1 << ". " << frequencies[i].real() << " + " << frequencies[i].imag() << "*i" << std::endl;
+		std::vector<std::complex<T>> frequencies;
+		constexpr unsigned int i_max = 150;
+		clock_t recursiveBase = clock();
+		for (unsigned int i = 0; i < i_max; i++) {
+			frequencies = directTransform(functionSamples);
 		}
-	}
+		recursiveBase = clock() - recursiveBase;
 
-	std::vector<T> testFunctionSamples(sampleNum);
-	double tempFill = 0;
-	for (auto& item : testFunctionSamples) {
-		item = tempFill;
-		tempFill += 1.0;
-	}
+		std::vector<std::complex<T>> complexFunctionSamples = Fourier::utils::realToComplex(functionSamples);
+		clock_t recursiveOptimized = clock();
+		for (unsigned int i = 0; i < i_max; i++) {
+			frequencies = directTransform(complexFunctionSamples);
+		}
+		recursiveOptimized = clock() - recursiveOptimized;
 
-	//std::cout << std::endl << "Test samples: " << std::endl;
-	//for (unsigned int i = 0; i < testFunctionSamples.size(); i++) {
-	//	std::cout << i + 1 << ". " << testFunctionSamples[i] << std::endl;
-	//}
+		clock_t nonrecursive = clock();
+		for (unsigned int i = 0; i < i_max; i++) {
+			frequencies = directTransformNonrecursive(complexFunctionSamples);
+		}
+		nonrecursive = clock() - nonrecursive;
+
+		std::cout << std::endl << "Base time: " << recursiveBase << std::endl 
+			<< "Optimized recursive time: " << recursiveOptimized << std::endl 
+			<< "Nonrecursive time: " << nonrecursive << std::endl;
+	}
 }
+
+
+
+//std::cout << "Input function: " << std::endl;
+//for (unsigned int i = 0; i < functionSamples.size(); i++) {
+//	std::cout << i + 1 << ". " << functionSamples[i] << std::endl;
+//}
+//
+//{
+//	using namespace Fourier::trivial;
+//	std::vector<std::complex<T>> frequencies =
+//		directTransform(functionSamples, deepth);
+//	//std::vector<T> restored = inverseTransform(frequencies, sampleNum);
+//
+//	std::cout << std::endl << "Direct Fourier transform: " << std::endl;
+//	for (unsigned int i = 0; i < frequencies.size(); i++) {
+//		std::cout << i + 1 << ". " << frequencies[i].real() << " + " << frequencies[i].imag() << "*i" << std::endl;
+//	}
+//	//std::cout << std::endl << "Function restoration with inverse Fourier transform: " << std::endl;
+//	//for (unsigned int i = 0; i < restored.size(); i++) {
+//	//	std::cout << i + 1 << ". " << restored[i] << std::endl;
+//	//}
+//}
--- Fourier.h
+++ Fourier.h
@@ -20,15 +20,15 @@ namespace Fourier {
 
 	namespace fast {
 		template <typename T>
-		std::vector<std::complex<T>> directTransform(const std::vector<T>& x, unsigned int k);
+		std::vector<std::complex<T>> directTransform(const std::vector<T>& x);
 
 		// Recursive optimization overload
 		template <typename T>
-		std::vector<std::complex<T>> directTransform(const std::vector<T>& x);
+		std::vector<std::complex<T>> directTransform(const std::vector<std::complex<T>>& x);
 
 		// Should be the fastest version
 		template <typename T>
-		std::vector<std::complex<T>> directTransformNonrecursive(const std::vector<T>& x);
+		std::vector<std::complex<T>> directTransformNonrecursive(const std::vector<std::complex<T>>& x);
 
 		template <typename T>
 		std::vector<T> inverseTransform(std::vector<std::complex<T>> c, unsigned int n);
@@ -47,8 +47,8 @@ namespace Fourier {
 						factor[i] = factor[i - 1] * exp;
 					}
 			
-					std::valarray<std::complex<T>>&& tempOdd = recursiveFourier<T>(std::valarray<T>(source[std::slice(1, halfLen, 2)])) * factor;
-					std::valarray<std::complex<T>>&& tempEven = recursiveFourier<T>(std::valarray<T>(source[std::slice(0, halfLen, 2)]));
+					std::valarray<std::complex<T>>&& tempOdd = recursiveDirect<T>(std::valarray<T>(source[std::slice(1, halfLen, 2)])) * factor;
+					std::valarray<std::complex<T>>&& tempEven = recursiveDirect<T>(std::valarray<T>(source[std::slice(0, halfLen, 2)]));
 			
 					result[std::slice(0, halfLen, 1)] = tempEven + tempOdd;
 					result[std::slice(halfLen, halfLen, 1)] = tempEven - tempOdd;
@@ -68,8 +68,8 @@ namespace Fourier {
 					std::slice inputEven(source.start(), halfLen, source.stride() * 2);
 					std::slice inputOdd(source.start() + source.stride(), halfLen, source.stride() * 2);
 
-					std::gslice leftPart = tempMatrix<T>(dist, inputEven);
-					std::gslice rightPart = tempMatrix<T>(dist, inputOdd);
+					std::gslice leftPart = recursiveDirect<T>(dist, inputEven);
+					std::gslice rightPart = recursiveDirect<T>(dist, inputOdd);
 
 
 					std::valarray<std::complex<T>> factor(-1, halfLen);
@@ -100,6 +100,17 @@ namespace Fourier {
 
 			template <typename T>
 			std::valarray<T> recursiveInverse(std::valarray<std::complex<T>> source);
+		}
+	}
+	namespace utils {
+		template <typename T>
+		std::vector<std::complex<T>> realToComplex(const std::vector<T>& input) {
+			size_t inputSize = input.size();
+			std::vector<std::complex<T>> result(inputSize);
+			for (size_t i = 0; i < inputSize; ++i) {
+				result[i] = std::complex<T>(input[i], 0);
+			}
+			return result;
 		}
 	}
 
@@ -138,7 +149,7 @@ std::vector<T> Fourier::trivial::inverseTransform(const std::vector<std::complex
 }
 
 template <typename T>
-std::vector<std::complex<T>> Fourier::fast::directTransform(const std::vector<T>& x, unsigned int k) {
+std::vector<std::complex<T>> Fourier::fast::directTransform(const std::vector<T>& x) {
 	std::valarray<std::complex<T>> result = Fourier::fast::recursiveDirect<T>(std::valarray<T>(x.data(), x.size()));
 	result /= x.size(); // something extremely strange
 	return std::vector<std::complex<T>>(std::begin(result), std::end(result));
@@ -151,11 +162,8 @@ std::vector<T> Fourier::fast::inverseTransform(std::vector<std::complex<T>> c, u
 }
 
 template <typename T>
-std::vector<std::complex<T>> Fourier::fast::directTransform(const std::vector<T>& x) {
-	std::valarray<std::complex<T>> result(x.size());
-	for (unsigned int i = 0; i < x.size(); i++) {
-		result[i] = std::complex<T>(x[i], 0);
-	}
+std::vector<std::complex<T>> Fourier::fast::directTransform(const std::vector<std::complex<T>>& x) {
+	std::valarray<std::complex<T>> result(x.data(), x.size());
 	result /= x.size();
 	std::slice inputValues(0, result.size(), 1);
 	std::valarray<std::complex<T>> ordered = result[Fourier::fast::recursiveDirect(result, inputValues)];
@@ -164,15 +172,12 @@ std::vector<std::complex<T>> Fourier::fast::directTransform(const std::vector<T>
 
 
 template <typename T>
-std::vector<std::complex<T>> Fourier::fast::directTransformNonrecursive(const std::vector<T>& x) {
+std::vector<std::complex<T>> Fourier::fast::directTransformNonrecursive(const std::vector<std::complex<T>>& x) {
 	size_t const inputSize = x.size();
 	if ((inputSize & (inputSize - 1)) != 0) {
 		throw std::logic_error("Invalid input vector size. ");
 	}
-	std::valarray<std::complex<T>> values(inputSize);
-	for (unsigned int i = 0; i < inputSize; i++) {
-		values[i] = std::complex<T>(x[i], 0);
-	}
+	std::valarray<std::complex<T>> values(x.data(), inputSize);
 	values /= inputSize;
 
 	// get binary logarithm for approximately O(1)
@@ -199,88 +204,30 @@ std::vector<std::complex<T>> Fourier::fast::directTransformNonrecursive(const st
 	
 	for (int i = dimensionNum - 1; i >= 0; --i) {
 		std::slice subarraySelection(i + 1, dimensionNum - i, 1);
-		std::slice factorSlice(0, inputSize / generalSlice.stride()[i] / 2, generalSlice.stride()[i]);
-		for (size_t j = 0; j < generalSlice.stride()[i]; j++) {
-			std::gslice leftSlice(j, generalSlice.size()[subarraySelection], generalSlice.stride()[subarraySelection]);
-			std::gslice rightSlice(j + generalSlice.stride()[i], generalSlice.size()[subarraySelection], generalSlice.stride()[subarraySelection]);
-	
-			values[rightSlice] *= factor[factorSlice];
+		std::valarray<size_t> currentSize = generalSlice.size()[subarraySelection];
+		std::valarray<size_t> currentStride = generalSlice.stride()[subarraySelection];
+
+		size_t maxStartIndex = generalSlice.stride()[i];
+		std::slice factorSlice(0, inputSize / maxStartIndex / 2, maxStartIndex);
+		std::valarray<std::complex<T>> factorTemp = factor[factorSlice];
+		for (size_t j = 0; j < maxStartIndex; j++) {
+			std::gslice leftSlice(j, currentSize, currentStride);
+			std::gslice rightSlice(j + maxStartIndex, currentSize, currentStride);
+
+			std::valarray<std::complex<T>> tempLeft = values[leftSlice];
+
+			values[rightSlice] *= factorTemp;
 			values[leftSlice] -= values[rightSlice];
-			// tricky way to multiply by 2 without unnecessary memory transactions, the same as values[rightSlice] *= 2;
- 			// unfortunately there is no way to multiply slice by scalar
-			values[rightSlice] += values[rightSlice]; 
-			values[rightSlice] += values[leftSlice];
+			values[rightSlice] += tempLeft;
+
+			//values[rightSlice] *= factor[factorSlice];
+			//values[leftSlice] -= values[rightSlice];
+			//// tricky way to multiply by 2 without unnecessary memory transactions, the same as values[rightSlice] *= 2;
+ 			//// unfortunately there is no way to multiply slice by scalar
+			//values[rightSlice] += values[rightSlice]; 
+			//values[rightSlice] += values[leftSlice];
 		}
 	}
 	std::valarray<std::complex<T>> ordered = values[generalSlice];
 	return std::vector<std::complex<T>>(std::begin(ordered), std::end(ordered));
 }
-
-
-
-// size_t totalRangeSize = source.size()[source.size().size() - 1] << source.size().size() - 1;
-// size_t totalRangeSize = 1;
-// for (auto val: source.size()) {
-// 	totalRangeSize *= val;
-// }
-
-//std::slice outputLow = std::slice(output.start(), halfLen, output.stride());
-//std::slice outputHigh = std::slice(output.start() + halfLen * output.stride(), halfLen, output.stride());
-
-//std::valarray<size_t> outputSizes(leftPart.size().size() + 1);
-//outputSizes[std::slice(0, leftPart.size().size(), 1)] = leftPart.size();
-//outputSizes[leftPart.size().size() - 1] = 2;
-//outputSizes[leftPart.size().size()] = halfLen;
-//
-//std::valarray<size_t> outputStrides(leftPart.stride().size() + 1);
-//outputStrides[std::slice(0, leftPart.stride().size(), 1)] = leftPart.stride();
-//outputStrides[leftPart.stride().size()] = outputStrides[leftPart.stride().size() - 1] * 2;
-
-//template <typename T>
-//std::valarray<std::complex<T>> recursiveFourier(std::valarray<T> source) {
-//	std::valarray<std::complex<T>> result(source.size());
-//	if (result.size() > 1) {
-//		size_t halfLen = source.size() / 2;
-//
-//		std::valarray<std::complex<T>> factor(1, halfLen);
-//		std::complex<T> exp = Fourier::exponentialFactor<T>(1, source.size());
-//		for (unsigned int i = 1; i < factor.size(); i++) {
-//			factor[i] = factor[i - 1] * exp;
-//		}
-//
-//		std::valarray<std::complex<T>>&& tempOdd = recursiveFourier<T>(std::valarray<T>(source[std::slice(1, halfLen, 2)])) * factor;
-//		std::valarray<std::complex<T>>&& tempEven = recursiveFourier<T>(std::valarray<T>(source[std::slice(0, halfLen, 2)]));
-//
-//		result[std::slice(0, halfLen, 1)] = tempEven + tempOdd;
-//		result[std::slice(halfLen, halfLen, 1)] = tempEven - tempOdd;
-//	}
-//	else {
-//		result[0] = std::complex<T>(source[0], 0);
-//	}
-//	return result;
-//}
-
-//template <typename T>
-//void temp(const std::valarray<T>& source, std::vector<std::complex<T>> dist, size_t start, size_t size, size_t stride) {
-//	if (size > 1) {
-//		size_t nextStride = stride * 2;
-//		size_t nextSize = size / 2;
-//
-//		temp(source, dist, start, nextSize, nextStride);
-//		temp(source, dist, start + stride, nextSize, nextStride);
-//
-//		std::complex<T> exp = Fourier::exponentialFactor<T>(1, size);
-//		std::complex<T> factor = 1;
-//
-//		size_t halfLinear = stride * (size - 1) / 2;
-//		for (size_t i = start; i < start + halfLinear; i += stride) {
-//			dist[i + halfLinear] *= factor;
-//			dist[i] -= dist[i + halfLinear];
-//			dist[i + halfLinear] = dist[i] + dist[i + halfLinear] * 2;
-//			factor *= exp;
-//		}
-//	}
-//	else {
-//		dist[start] = std::complex<T>(source[start], 0);
-//	}
-//}
